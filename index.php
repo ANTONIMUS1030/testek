b4ee4b18f669eb41032f2fca83227d3367216368
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-01-22 00:43:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-[
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfGSv5sKqOiq9sJ_cl1A-8abpGwOzqkgFaDQ&usqp=CAU",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm8aLA82fJF3w0v_-b6jk6bK_SeUvClnlCMw&usqp=CAU"
-]
+Dark domina 😏
